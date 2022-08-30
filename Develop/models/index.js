2cbc86ec0e7c80bf75472ac20be910c5b4e19491
Jeremy Johnson <jeremyjohnson@Jeremys-Mini.hsd1.ut.comcast.net>
2022-08-30 16:34:00 -0600

    finished associations in models/index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
 
 module.exports = {
   Product,
